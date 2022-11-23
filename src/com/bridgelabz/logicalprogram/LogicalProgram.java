d18e3d2a4d3f36f85985f341459be9fde50d6f81
shiwani <shiwanipaunikar1@gmail.com>
2022-11-23 10:45:49 -0800

    [shiwani]logical program are added

---
+++ src/com/bridgelabz/logicalprogram/LogicalProgram.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.logicalprogram;
+
+import java.util.Scanner;
+
+public class LogicalProgram {
+    public static void main(String[] args) {
+        System.out.println("logical Program");
+
+    }
+}
